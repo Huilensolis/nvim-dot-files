0e3a719c0afe8ad81ac37c08bc853f797576a54b
Huilensolis <xywvanz@gmail.com>
2023-12-24 21:15:08 -0300

    starter commit

---
+++ lua/plugins/colorshceme.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		opts = { flavour = "macchiato", transparent_background = true },
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				float = "transparent",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = { eslint = {} },
+			setup = {
+				eslint = function()
+					require("lazyvim.util").lsp.on_attach(function(client)
+						if client.name == "eslint" then
+							client.server_capabilities.documentFormattingProvider = true
+						elseif client.name == "tsserver" then
+							client.server_capabilities.documentFormattingProvider = false
+						end
+					end)
+				end,
+			},
+		},
+	},
+}
---
+++ lua/plugins/editor.lua
@@ -0,0 +1,204 @@
+return {
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		keys = function()
+			return {}
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-emoji",
+		},
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api
+							.nvim_buf_get_lines(0, line - 1, line, true)[1]
+							:sub(col, col)
+							:match("%s")
+						== nil
+			end
+
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+
+			opts.mapping = vim.tbl_extend("force", opts.mapping, {
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+						cmp.select_next_item()
+					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+					-- this way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = { eslint = {} },
+			setup = {
+				eslint = function()
+					require("lazyvim.util").lsp.on_attach(function(client)
+						if client.name == "eslint" then
+							client.server_capabilities.documentFormattingProvider = true
+						elseif client.name == "tsserver" then
+							client.server_capabilities.documentFormattingProvider = false
+						end
+					end)
+				end,
+			},
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		},
+		opts = {
+			options = {
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(_, _, diag)
+					local icons = require("lazyvim.config").icons.diagnostics
+					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+			-- Fix bufferline when restoring a session
+			vim.api.nvim_create_autocmd("BufAdd", {
+				callback = function()
+					vim.schedule(function()
+						pcall(nvim_bufferline)
+					end)
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-cmp",
+		dependencies = {
+			-- codeium
+			{
+				"Exafunction/codeium.nvim",
+				cmd = "Codeium",
+				build = ":Codeium Auth",
+				opts = {},
+			},
+		},
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			table.insert(opts.sources, 1, {
+				name = "codeium",
+				group_index = 1,
+				priority = 100,
+			})
+		end,
+	},
+	{
+		"Exafunction/codeium.nvim",
+		cmd = "Codeium",
+		build = ":Codeium Auth",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				tailwindcss = {
+					-- exclude a filetype from the default_config
+					filetypes_exclude = { "markdown" },
+					-- add additional filetypes to the default_config
+					filetypes_include = {},
+					-- to fully override the default_config, change the below
+					-- filetypes = {}
+				},
+			},
+			setup = {
+				tailwindcss = function(_, opts)
+					local tw = require("lspconfig.server_configurations.tailwindcss")
+					opts.filetypes = opts.filetypes or {}
+
+					-- Add default filetypes
+					vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+					-- Remove excluded filetypes
+					--- @param ft string
+					opts.filetypes = vim.tbl_filter(function(ft)
+						return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+					end, opts.filetypes)
+
+					-- Add additional filetypes
+					vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+				end,
+			},
+		},
+	},
+}
